e09a10ae4367d18928a1f4c51502946b6fadd0ff
Simon Si <283736566@qq.com>
2024-06-18 18:37:11 +0800

    using custom parameters

--- features/step_definitions/step.js
+++ features/step_definitions/step.js
@@ -2,12 +2,15 @@ const { Given, When, Then } = require("@cucumber/cucumber");
 const { assertThat, is, not } = require("hamjest");
 const Person = require("../../src/shouty");
 
-Given("Lucy is located/standing {int} meters from Sean", function (distance) {
-  this.lucy = new Person("Lucy");
-  this.sean = new Person("Sean");
-  this.distance = distance;
-  this.lucy.moveTo(distance);
-});
+Given(
+  "{person} is located/standing {int} meters from Sean",
+  function (lucy, distance) {
+    this.lucy = lucy;
+    this.sean = new Person("Sean");
+    this.distance = distance;
+    this.lucy.moveTo(distance);
+  }
+);
 
 When("Sean shouts {string}", function (message) {
   this.sean.shout(message);
